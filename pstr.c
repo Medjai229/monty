68bf3abdfbc0b810902e63159f1fc79de5dd588a
Malik <malikhussein229@gmail.com>
2024-01-18 14:28:51 -0800

    Adding functions

---
+++ pstr.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * s_pstr -  prints the string starting at the top of the stack
+ * @head: pointer of the stack head
+ * @counter: line number
+ */
+void s_pstr(stack_t **head, unsigned int counter)
+{
+	(void)counter;
+	stack_t *tmp;
+
+	tmp = *head;
+	while (tmp)
+	{
+		if (tmp->n <= 0 || tmp->n > 127)
+			break;
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
