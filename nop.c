04881c97613037d2cc559b46892c1d29f0919ba1
Malik <malikhussein229@gmail.com>
2024-01-18 12:41:49 -0800

    Adding functions

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * s_nop - doesn’t do anything
+ * @head: pointer of the stack head
+ * @counter: line number
+ */
+void s_nop(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+}
