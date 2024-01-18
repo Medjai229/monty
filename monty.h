7d2bd69bc1273e8ba5917dcc9932719f3358107f
Malik <malikhussein229@gmail.com>
2024-01-18 14:48:29 -0800

    Updating Prototypes

--- monty.h
+++ monty.h
@@ -74,5 +74,6 @@ void s_mul(stack_t **head, unsigned int counter);
 void s_mod(stack_t **head, unsigned int counter);
 void s_pchar(stack_t **head, unsigned int counter);
 void s_pstr(stack_t **head, unsigned int counter);
+void s_rotl(stack_t **head, unsigned int counter);
 
 #endif /* MONTY_H */
