27aa95d7f9385f0531bbb19d1f205205f740d255
Malik <malikhussein229@gmail.com>
2024-01-18 15:01:19 -0800

    Adding functions

---
+++ queue.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ *
+ */
+void q_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
---
+++ stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * s_stack - switch to a stack
+ * @head: pointer of the stack head
+ * @counter: line number
+ */
+void s_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
